421e9299dbeeb36c77a6490736c7c6505b4858e8
divnaoki <naokiimai1@gmail.com>
2022-06-13 21:50:28 +0900

    Blogモデル作成

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
+class Blog(models.Model):
+  title = models.CharField(null=False, max_length=120)
+  content = models.TextField(null=False)
+  create_at = models.DateTimeField(default=timezone.now)
+  update_at = models.DateTimeField(default=timezone.now)
