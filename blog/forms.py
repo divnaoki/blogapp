b1ddc0201ac7fedb7e709bddfd4d38cbb950eec6
divnaoki <naokiimai1@gmail.com>
2022-06-19 19:44:27 +0900

    Category選択forms.py修正

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,6 @@
-from turtle import width
-from attr import attr, field
 from django import forms
 from .models import Blog
+from category.models import Category
 
 class BlogForm(forms.ModelForm):
   title = forms.CharField(
@@ -20,9 +19,15 @@ class BlogForm(forms.ModelForm):
       }
     )
   )
+  category = forms.ModelChoiceField(
+    label='category',
+    queryset=Category.objects,
+    required=False
+  )
   class Meta:
     model = Blog
     fields = [
       "title",
-      "content"
+      "content",
+      "category"
     ]
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from category.models import Category
 class Blog(models.Model):
   title = models.CharField(null=False, max_length=120)
   content = models.TextField(null=False)
   create_at = models.DateTimeField(default=timezone.now)
   update_at = models.DateTimeField(default=timezone.now)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
